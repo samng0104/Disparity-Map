ad63dca6508715a507532529111140536209fab9
Lung Ping NG <nglungping@gmail.com>
2023-05-01 14:24:34 +0800

    best result

--- main.py
+++ main.py
@@ -9,40 +9,14 @@ Compare to the ground truth
 """
 import cv2
 import numpy as np
-
-def display_image(left_image, right_image):
-
-    if left_image is None or right_image is None:
-        print('Failed to load the images')
-        exit(1)
-
-    cv2.imshow('Left Image', left_image)
-    cv2.imshow('Right Image', right_image)
-
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-def convert_grayscale(left_image, right_image):
-    return cv2.cvtColor(left_image, cv2.COLOR_BGR2GRAY), cv2.cvtColor(right_image, cv2.COLOR_BGR2GRAY)
+from PIL import Image
 
 def compute_disparity(left_image, right_image):
-    window_size = 3
-    min_disp = 0
-    max_disp = 16
 
-    # Create a StereoBM object
-    stereo = cv2.StereoSGBM_create(minDisparity=min_disp,
-                                numDisparities=max_disp,
-                                blockSize=window_size,
-                                P1=8*3*window_size**2,
-                                P2=32*3*window_size**2,
-                                disp12MaxDiff=1,
-                                uniquenessRatio=10,
-                                speckleWindowSize=100,
-                                speckleRange=32)
+    stereo = cv2.StereoBM_create(numDisparities=16, blockSize=5)
     # Compute the disparity map
     disparity_map = stereo.compute(left_image, right_image)
-    disparity_map = cv2.normalize(disparity_map, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+    # disparity_map = cv2.normalize(disparity_map, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
     return disparity_map
 
 if __name__ == '__main__':
@@ -53,14 +27,20 @@ if __name__ == '__main__':
         left_image_name = r'./Stereo_Images/' + Stereo_Images[i] + r'/view1.png'
         right_iamge_name = r'./Stereo_Images/' + Stereo_Images[i] + r'/view5.png'
 
-        left_image = cv2.imread(left_image_name, cv2.IMREAD_COLOR)
-        right_image = cv2.imread(right_iamge_name, cv2.IMREAD_COLOR)
+        left_image = np.asarray(Image.open(left_image_name).convert('L'))
+        right_image = np.asarray(Image.open(right_iamge_name).convert('L'))
 
-        # Check if the images are loaded successfully
-        # display_image(left_image, right_image)
+        if Stereo_Images[i] == 'Art':
+            left_image = cv2.GaussianBlur(left_image, (5, 5), 0)
+            right_image = cv2.GaussianBlur(right_image, (5, 5), 0)
+        else:
+            left_image = cv2.GaussianBlur(left_image, (7, 7), 0)
+            right_image = cv2.GaussianBlur(right_image, (7, 7), 0)
 
-        # convert_grayscale(left_image, right_image)
 
-        reconstructed_result = compute_disparity(left_image, right_image)
+        pred_disp = compute_disparity(left_image, right_image)
+
+        pred_disp = cv2.GaussianBlur(pred_disp, (19, 19), 0)
+
         disp_name = r'./pred/' + Stereo_Images[i] + r'/disp1.png'
-        cv2.imwrite(disp_name, reconstructed_result)
+        cv2.imwrite(disp_name, pred_disp)
